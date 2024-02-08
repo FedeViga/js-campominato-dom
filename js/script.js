5dd76b597381cfeceb4f2d56173aab1f9fb59325
federico_viganò <fede_viga@hotmail.it>
2024-02-08 13:36:06 +0100

    creata funzione per generare l'array di numeri casuali

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 /*
 Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Potete realizzare lo stile che volete, sin quando tutto è leggibile con facilità
-Bonus
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco 
+(attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+Il computer deve generare 16 numeri casuali e inserirli in un array, in base al range della difficoltà prescelta 
+(se abbiamo scelto facile l'array conterrà numeri casuali da 1 a 100, se invece abbiamo scelto difficile l'array dovrà contenerne da 1 a 49): questi rappreseranno le posizioni delle nostre bombe :bomba:.
+Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+BONUS 1
+Quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle.
+BONUS 2
+Quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste.
 */
 
 // inizializzo elementi HTML
 const buttonElement = document.querySelector("#play");
 const gridElement = document.querySelector("#grid");
+let gridSize = 0;
 
 // creo funzione che genera un numero diverso di caselle in base alla difficoltà scelta dall'utente
 function gridGenerator(number) {
@@ -48,36 +51,60 @@ function gridGenerator(number) {
 }
 
 
+
+// creo funzione che genera un array di arrayLength numeri casuali da 1 a 16 tutti diversi tra loro
+function randomNumbersArray(max) {
+    const numbersArray = [];
+
+    while (numbersArray.length < 16) {
+        const RandomNumber = Math.floor(Math.random() * max + 1);
+         if (! numbersArray.includes(RandomNumber)) {
+            numbersArray.push(RandomNumber);
+         }
+    }
+    return numbersArray;
+}
+
+
 // al click del bottone genero una griglia N celle in base alla difficoltà
 buttonElement.addEventListener('click', function() {
 
     console.clear();
     gridElement.innerHTML = "";
     const selectElement = document.querySelector("#select");
-    let gridSize;
 
-    // livello facile
-    if (selectElement.value == 3) {
-        gridSize = 49;
-        gridElement.classList.add("w-700");
-        gridElement.classList.remove("w-900");
-        gridElement.classList.remove("w-1000");
+    if (selectElement.value == 0) {
+        const addDifficulty = document.createElement("h2");
+        addDifficulty.innerHTML = "Scegli la Difficoltà per giocare"
+        gridElement.append(addDifficulty);
 
-    // livello normale 
-    } else if (selectElement.value == 2) {
-        gridSize = 81;
-        gridElement.classList.remove("w-700");
-        gridElement.classList.add("w-900");
-        gridElement.classList.remove("w-1000");
+    } else {
 
-    // livello difficile  
-    } else if (selectElement.value == 1) {
-        gridSize = 100;
-        gridElement.classList.remove("w-700");
-        gridElement.classList.remove("w-900");
-        gridElement.classList.add("w-1000");
+        // livello facile
+        if (selectElement.value == 3) {
+            gridSize = 49;
+            gridElement.classList.add("w-700");
+            gridElement.classList.remove("w-900");
+            gridElement.classList.remove("w-1000");
+    
+        // livello normale 
+        } else if (selectElement.value == 2) {
+            gridSize = 81;
+            gridElement.classList.remove("w-700");
+            gridElement.classList.add("w-900");
+            gridElement.classList.remove("w-1000");
+    
+        // livello difficile  
+        } else if (selectElement.value == 1) {
+            gridSize = 100;
+            gridElement.classList.remove("w-700");
+            gridElement.classList.remove("w-900");
+            gridElement.classList.add("w-1000");
+        }
+    
+        gridGenerator(gridSize);
+        console.log(randomNumbersArray(gridSize));
     }
 
-    gridGenerator(gridSize);
 
 })
